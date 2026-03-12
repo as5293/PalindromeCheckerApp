96002776325195a27f6b7a087ba29def85c4e935
amitha <as5293@srmist.edu.in>
2026-03-12 18:52:05 +0530

    UC13:Palindrome Checker App

--- src/PalindromCheckerApp.java
+++ src/PalindromCheckerApp.java
@@ -1,35 +1,68 @@
 import java.util.Scanner;
+import java.util.Stack;
 
-public class PalindromeCheckerApp {
+public class PalindromCheckerApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Input: ");
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().toLowerCase();
 
-        PalindromeService service = new PalindromeService();
-        boolean result = service.checkPalindrome(input);
+        PalindromeStrategy stackStrategy = new StackStrategy();
+        PalindromeStrategy twoPointerStrategy = new TwoPointerStrategy();
 
-        System.out.println("Is Palindrome?: " + result);
+        long start1 = System.nanoTime();
+        boolean result1 = stackStrategy.check(input);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        boolean result2 = twoPointerStrategy.check(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("Stack Strategy Result: " + result1);
+        System.out.println("Stack Execution Time: " + (end1 - start1) + " ns");
+
 
         scanner.close();
     }
 }
 
-class PalindromeService {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 
-    public boolean checkPalindrome(String input) {
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class TwoPointerStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
 
         int start = 0;
         int end = input.length() - 1;
 
         while (start < end) {
-
             if (input.charAt(start) != input.charAt(end)) {
                 return false;
             }
-
             start++;
             end--;
         }
